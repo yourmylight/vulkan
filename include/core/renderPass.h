9eb7495cc1e7b02f68bdf3f146da3e4990cfd2b2
Wind <2769323502@qq.com>
2025-11-25 23:34:25 +0800

    build renderpass

--- include/core/renderPass.h
+++ include/core/renderPass.h
@@ -5,8 +5,13 @@
 namespace core {
     class RenderPass {
     public:
-        RenderPass();
+        RenderPass(VkDevice _logicalDevice);
         ~RenderPass();
+        RenderPass& beginRenderPass(VkPipelineBindPoint bindPoint);
+        RenderPass& endSubpass();
+        RenderPass& addAttachment(const VkAttachmentDescription& attachmentDes);
+        RenderPass& addColorAttachment(uint32_t attachemnt, VkImageLayout layout);
+        void build(const std::vector<VkSubpassDependency>& subpassDependency);
     private:
         VkDevice logicalDevice{};
         std::vector<VkAttachmentDescription> attachmentDescription;
@@ -18,5 +23,7 @@ namespace core {
         std::vector<std::vector<VkAttachmentReference>> inputRefsPerSubpass;
         std::vector<std::vector<VkAttachmentReference>> resolveRefsPerSubpass;
         std::vector<std::vector<uint32_t>> preserveRefsPerSubpass;
+
+        VkRenderPass renderPass;
     };
 }
--- src/core/renderPass.cpp
+++ src/core/renderPass.cpp
@@ -1,11 +1,40 @@
 #include "core/renderPass.h"
 
 namespace core {
-    RenderPass::RenderPass() {
+    RenderPass::RenderPass(VkDevice _logicalDevice) : logicalDevice(_logicalDevice)
+    {
 
     }
 
     RenderPass::~RenderPass() {
+        if (renderPass != VK_NULL_HANDLE) {
+            vkDestroyRenderPass(logicalDevice, renderPass, nullptr);
+        }
+    }
+
+    RenderPass& RenderPass::beginRenderPass(VkPipelineBindPoint bindPoint) {
+        colorRefsPerSubpass.emplace_back();
+        VkSubpassDescription subpass{
+            .pipelineBindPoint = bindPoint
+        };
+        subpassDescription.push_back(subpass);
+    }
+
+    RenderPass& endSubpass() {
         
+    }
+
+    RenderPass& RenderPass::addColorAttachment(uint32_t attachemnt, VkImageLayout layout) {
+        colorRefsPerSubpass.back().emplace_back(VkAttachmentReference{attachemnt, layout});
+    }
+
+    RenderPass& RenderPass::addAttachment(const VkAttachmentDescription& attachmentDes) {
+        attachmentDescription.emplace_back(attachmentDes);
+    }
+
+    void RenderPass::build(const std::vector<VkSubpassDependency>& subpassDependency) {
+        VkRenderPassCreateInfo createInfo {
+
+        };
     }
 }
